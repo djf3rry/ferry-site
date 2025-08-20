ecc57fc6070feb64a54c07ee8412d6ae206a52f0
djf3rry <djf3rry@gmail.com>
2025-08-20 00:47:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("name").textContent = siteConfig.name;
-  document.getElementById("bioText").textContent = siteConfig.bio;
-});
